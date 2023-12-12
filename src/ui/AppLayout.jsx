41b98dcd52d5cec545b8023ab7981ba5d9d0ba58
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-12-12 13:46:53 +0100

    update

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,29 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import SideBar from "./SideBar";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: green;
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
-    <div>
-      <SideBar />
+    <StyledAppLayout>
       <Header />
-      <main>
+      <SideBar />
+      <Main>
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 }
 
--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
+const StyledSideBar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey);
+
+  grid-row: 1 / -1;
+`;
+
 function SideBar() {
-  return <aside>SIDEBAR</aside>;
+  return <StyledSideBar>SIDEBAR</StyledSideBar>;
 }
 
 export default SideBar;
