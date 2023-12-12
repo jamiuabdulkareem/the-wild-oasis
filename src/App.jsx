83868db5e17b116e13120e1c1ecd4242dfecac95
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-12-12 01:33:16 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
+  background-color: yellow;
 `;
 
 function App() {
   return (
-    <div>
-      <H1>Hello word</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <H1>The Wild Oasis</H1>
+      <Button>Check in</Button>
+      <Input type="number" placeholder="Number of guest" />
+    </>
   );
 }
 
