285bcab616d47752f76861907e8c0af1a408bb2a
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-12-12 02:10:42 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,36 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import styled from "styled-components";
+import Row from "./ui/Row";
+
+const StylesApp = styled.main`
+  padding: 20px;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Heading type="h1">The Wild Oasis</Heading>
+      <StylesApp>
+        <Row type="horizontal">
+          <Heading as="h1">The Wild Oasis</Heading>
 
-      <Heading type="h2">Check in and out</Heading>
-      <Button>Check in</Button>
-      <Button>Check out</Button>
-      <Input type="number" placeholder="Number of guest" />
+          <div>
+            <Heading as="h2">Check in and out</Heading>
+            <Button>Check in</Button>
+            <Button>Check out</Button>
+          </div>
+        </Row>
 
-      <Heading type="h3">Form</Heading>
-      <Input type="number" placeholder="Number of guest" />
+        <Row type="vertical">
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number of guest" />
+            <Input type="number" placeholder="Number of guest" />
+          </form>
+        </Row>
+      </StylesApp>
     </>
   );
 }
