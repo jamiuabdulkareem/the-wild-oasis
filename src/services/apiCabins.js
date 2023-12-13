38fa335bab35cabc98b77765c6fdc20fcc1d538d
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-12-13 02:31:31 +0100

    update

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,1 +1,23 @@
-function getCabins() {}
+import supabase from "./supabase";
+
+export default async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not been loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not been delete");
+  }
+
+  return data;
+}
